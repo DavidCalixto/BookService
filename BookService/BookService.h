57c080d585c5545b365db6188abe5c5120e7247c
davidIco11 <56972468+DavidCalixto@users.noreply.github.com>
2019-12-28 15:18:53 -0600

    Initial Commit

---
+++ BookService/BookService.h
@@ -0,0 +1,19 @@
+//
+//  BookService.h
+//  BookService
+//
+//  Created by David Calixto on 28/12/19.
+//  Copyright © 2019 David Calixto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BookService.
+FOUNDATION_EXPORT double BookServiceVersionNumber;
+
+//! Project version string for BookService.
+FOUNDATION_EXPORT const unsigned char BookServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BookService/PublicHeader.h>
+
+
