d7f3c66dd0c3be76b5db78e1e8ddcbcb6268ac3d
davidIco11 <56972468+DavidCalixto@users.noreply.github.com>
2019-12-28 16:01:12 -0600

    Path added to Endpoint

--- BookServiceTests/EndPointTest.swift
+++ BookServiceTests/EndPointTest.swift
@@ -27,11 +27,17 @@ class EndPointTest: XCTestCase {
     func testEndpoint_has_a_validUrl() {
         assertValidURL(sut.url)
     }
+    
+    func testadd_a_path_to_host() {
+        sut.path = "books/api"
+        assertValidURL(sut.url)
+    }
+
 }
 
 struct Endpoint {
     let url: URL?
-    
+    var path: String = ""
     init (_ url : URL?) {
         self.url = url
     }
