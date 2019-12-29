e3566893fa780fb9ad38c96bc2793560d3089733
davidIco11 <56972468+DavidCalixto@users.noreply.github.com>
2019-12-28 20:38:43 -0600

    ImageCache added to inMemoryImageLoader

--- BookServiceTests/ImageLoaderTests.swift
+++ BookServiceTests/ImageLoaderTests.swift
@@ -41,13 +41,49 @@ protocol ImageLoader: class {
     associatedtype key: Hashable
     func data(for id: key) -> AnyPublisher<Data?, Never >
 }
-class InMemoryImageLoader: ImageLoader {
-    var persistence: [String: Data] = [:]
+final class InMemoryImageLoader: ImageLoader {
+    
+    private var persistence: ImageCache = ImageCache()
+    
     func save(_ data: Data, for key: String) {
         persistence[key] = data
     }
+    
     func data(for id: String) -> AnyPublisher<Data?, Never > {
         let data = persistence[id]
         return Just(data).eraseToAnyPublisher()
+    }
+    
+    final private class ImageCache {
+        private let theardlock = NSLock()
+        private lazy var cache: NSCache<AnyObject, AnyObject> = {
+            let cache = NSCache<AnyObject, AnyObject>()
+            cache.countLimit = 1024 * 1024 * 100
+            return cache
+        }()
+        
+        func get(_ key: String) -> Data? {
+            theardlock.lock(); defer { theardlock.unlock() }
+            return cache.object(forKey: key as AnyObject) as? Data
+        }
+        
+        func set(_ data: Data?, for key: String) {
+            theardlock.lock(); defer { theardlock.unlock() }
+            guard let object = data else { return removeData(for: key) }
+            cache.setObject(object as AnyObject, forKey: key as AnyObject)
+        }
+        
+        func removeData(for key: String) {
+            cache.removeObject(forKey: key as AnyObject)
+        }
+        
+        subscript(_ key: String) -> Data? {
+            get {
+                return get(key)
+            }
+            set {
+                return set(newValue, for: key)
+            }
+        }
     }
 }
