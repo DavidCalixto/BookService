a3131f356809cb4aca82be94703279b7c2bee718
davidIco11 <56972468+DavidCalixto@users.noreply.github.com>
2019-12-30 01:02:00 -0600

    Booksearcher added

---
+++ BookServiceTests/BookSearcherTests.swift
@@ -0,0 +1,27 @@
+//
+//  BookSearcherTests.swift
+//  BookServiceTests
+//
+//  Created by David Calixto on 30/12/19.
+//  Copyright © 2019 David Calixto. All rights reserved.
+//
+
+import XCTest
+
+class BookSearcherTests: XCTestCase {
+
+    var sut: BookSearcher!
+    
+    override func setUp() {
+        sut = BookSearcher()
+    }
+
+    override func tearDown() {
+        sut = nil
+    }
+
+}
+
+class BookSearcher {
+    
+}
