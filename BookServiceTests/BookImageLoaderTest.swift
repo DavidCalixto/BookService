efbb79557550188a55d8feb735466fb4f57da0bc
davidIco11 <56972468+DavidCalixto@users.noreply.github.com>
2019-12-29 20:03:48 -0600

    BookImageloader test added

---
+++ BookServiceTests/BookImageLoaderTest.swift
@@ -0,0 +1,17 @@
+//
+//  BookImageLoaderTest.swift
+//  BookServiceTests
+//
+//  Created by David Calixto on 29/12/19.
+//  Copyright © 2019 David Calixto. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import BookService
+class BookImageLoaderTest: XCTestCase {
+    func testNothing(){
+        
+    }
+}
+
