a4fa9b9e5158fde12368c3b325b671d82b841753
davidIco11 <56972468+DavidCalixto@users.noreply.github.com>
2019-12-30 09:26:48 -0600

    BookSearcher has been removed from test

--- BookServiceTests/BookSearcherTests.swift
+++ BookServiceTests/BookSearcherTests.swift
@@ -43,30 +43,3 @@ class BookSearcherTests: XCTestCase {
     }
 
 }
-
-enum APIError: Error {
-    case noService
-    case noData
-}
-
-class BookSearcher {
-
-    func searh(_ query: String) -> AnyPublisher<Data, APIError> {
-        return getRemotePublisher(query)
-    }
-        func getRemotePublisher(_  query: String) -> AnyPublisher< Data, APIError> {
-            guard let url = BookEndpoint.bookSearch(query) else { fatalError("URLs cannot be created") }
-            return dataTaskPublisher(url)
-        }
-            fileprivate func dataTaskPublisher(_ url: URL) -> AnyPublisher< Data, APIError> {
-               return URLSession.shared.dataTaskPublisher(for: url)
-                .map{ return $0.data }
-                .mapError { (error) -> APIError in
-                    
-                    if error.networkUnavailableReason != nil {
-                        return APIError.noService
-                    }
-                    return APIError.noData
-                    }.eraseToAnyPublisher()
-            }
-}
