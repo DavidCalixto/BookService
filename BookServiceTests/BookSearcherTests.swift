e19085d0bb90175ffc97116c87a6b6fb112e6560
davidIco11 <56972468+DavidCalixto@users.noreply.github.com>
2019-12-30 09:24:12 -0600

    Remote APICAll Added

--- BookServiceTests/BookSearcherTests.swift
+++ BookServiceTests/BookSearcherTests.swift
@@ -7,7 +7,8 @@
 //
 
 import XCTest
-
+import Combine
+@testable import BookService
 class BookSearcherTests: XCTestCase {
 
     var sut: BookSearcher!
@@ -19,9 +20,59 @@ class BookSearcherTests: XCTestCase {
     override func tearDown() {
         sut = nil
     }
+    
+    func testCanHandleAQuery(){
+        sut.searh("")
+    }
+    
+    
+    
+    func testGivenAQuery_should_callRemoteAPI() {
+        
+        let expectation = XCTestExpectation()
+        var cancelables: [AnyCancellable] = []
+        let cancellable = sut.searh("roberto").sink(receiveCompletion: { completion in
+            switch completion {
+            case .finished:
+                expectation.fulfill()
+                break
+            case .failure(let apiError):
+                XCTFail(apiError.localizedDescription)
+            }
+        }) { (data) in
+            expectation.fulfill()
+            XCTAssertNotNil(data)
+        }
+        cancellable.store(in: &cancelables)
+        
+        wait(for: [expectation], timeout: 5)
+    }
 
 }
 
+enum APIError: Error {
+    case noService
+    case noData
+}
+
 class BookSearcher {
-    
+
+    func searh(_ query: String) -> AnyPublisher<Data, APIError> {
+        return getRemotePublisher(query)
+    }
+        func getRemotePublisher(_  query: String) -> AnyPublisher< Data, APIError> {
+            guard let url = BookEndpoint.bookSearch(query) else { fatalError("URLs cannot be created") }
+            return dataTaskPublisher(url)
+        }
+            fileprivate func dataTaskPublisher(_ url: URL) -> AnyPublisher< Data, APIError> {
+               return URLSession.shared.dataTaskPublisher(for: url)
+                .map{ return $0.data }
+                .mapError { (error) -> APIError in
+                    
+                    if error.networkUnavailableReason != nil {
+                        return APIError.noService
+                    }
+                    return APIError.noData
+                    }.eraseToAnyPublisher()
+            }
 }
